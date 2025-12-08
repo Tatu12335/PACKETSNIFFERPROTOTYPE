40e1537277c4bb14fcf33e9a8e805028ad56ccea
tatus <tatus@MEIK>
2025-12-08 13:43:36 +0200

    Added logic for example : saving to a file which is still work in progress, determining whether a packe t is ipv4 or ipv6 and figuring out the protocol.

--- PACKETSNIFFERPROTOTYPE/Program.cs
+++ PACKETSNIFFERPROTOTYPE/Program.cs
@@ -1,9 +1,9 @@
-﻿using PacketDotNet;
+﻿
+// Dependencies
 using PcapDotNet.Core;
 using System.Net;
 using System.Diagnostics;
 using System.Collections.Generic;
-using PcapDotNet = PcapDotNet.Packets.Packet;
 using PcapDotNet.Core.Extensions;
 using System.Xml.Linq;
 using PcapDotNet.Packets;
@@ -21,6 +21,20 @@ using PcapDotNet.Packets.Icmp;
 using Microsoft.VisualBasic.FileIO;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection.PortableExecutable;
+using System.Threading.Channels;
+using PacketDotNet;
+using System.Threading;
+
+
+
+
+
+
+
+
+// End Dependencies
+
+
 
 
 // This is a prototype for a packet sniffer application using PcapDotNet library
@@ -38,19 +52,18 @@ namespace PACKETSNIFFERPROTOTYPE
         static void Main(string[] args)
         {
 
-            // Handle Ctrl + C event to stop packet capture gracefully
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancelKeyPress);  
+           
 
             // Retrieve the device list
             IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
             if (allDevices.Count == 0)
             {
-                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                Console.WriteLine(" No interfaces found! Make sure WinPcap is installed.");
                 return;
             }
             else
             {
-                Console.WriteLine("The following interfaces are available:");
+                Console.WriteLine(" The following interfaces are available : ");
                 Console.WriteLine();
                 for (int i = 0; i != allDevices.Count; i++)
                 {
@@ -74,7 +87,7 @@ namespace PACKETSNIFFERPROTOTYPE
                 }
                 try
                 {
-                    // Call the start capture method with the selected device
+                    // Call the start capture method with the selected device method
                     StartCapture(allDevices[SelectDevice()]);
                 }
                 catch (FormatException)
@@ -87,7 +100,7 @@ namespace PACKETSNIFFERPROTOTYPE
                 }
                 finally
                 {
-                    // this is a placeholder for any cleanup code if needed in the future
+                    // This is a placeholder for any cleanup code if needed in the future
                     Console.WriteLine(" Finished processing device ");
 
                 }
@@ -112,7 +125,7 @@ namespace PACKETSNIFFERPROTOTYPE
             // Checks if the device index is valid
             if (deviceIndex < 0 || deviceIndex >= LivePacketDevice.AllLocalMachine.Count)
             {
-                throw new ArgumentOutOfRangeException("Device index is out of range.");
+                throw new ArgumentOutOfRangeException(" Device index is out of range.");
             }
             // Returns the selected device index
             else
@@ -128,22 +141,33 @@ namespace PACKETSNIFFERPROTOTYPE
 
             // Placeholder for starting packet capture on the selected device
             Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine($" Starting capturing packets on : {device.Name} ctrl + c to stop");
-            
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // Handle Ctrl + C event to stop packet capture gracefully
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancelKeyPress);
+
+
             while (!_IsCancelled)
             {
+                
+                Thread.Sleep(1000); // Sleep for a short duration to prevent high CPU usage
 
-                using(PacketCommunicator communicator = device.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))
+                using (PacketCommunicator communicator = device.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))
                 {
                     // Start the capture
                     communicator.ReceivePackets(0, packet =>
                     {
 
                         IpV4Datagram ipv4Packet = packet.Ethernet.IpV4;
+                        IpV6Datagram ipv6Packet = packet.Ethernet.IpV6;
+                        packet.DataLink.ToString();
+
+                        MagicMethod(packet);
 
 
-                        // Process each packet
-                        Console.WriteLine($"Source : {ipv4Packet.Source} | Destinatination : {ipv4Packet.Destination} | Payload : {ipv4Packet.Payload} | Protocol  : {ipv4Packet.Protocol} |");
+
 
                     });
                     
@@ -157,28 +181,109 @@ namespace PACKETSNIFFERPROTOTYPE
 
 
         }
-        // Handle Ctrl + C event to stop packet capture gracefully and save data to desktop or documents.
-        private static void HandleCancelKeyPress(object? sender , ConsoleCancelEventArgs e)
+        // Handle Ctrl + C event to stop packet capture gracefully and save data to desktop or documents
+        private static void HandleCancelKeyPress(object? sender, ConsoleCancelEventArgs e)
         {
+            // Prevent the process from terminating immediately
             e.Cancel = true;
-            
 
+            // Save captured data to desktop or documents folder
             Console.WriteLine(" Saving captured data...");
             var curDir = FileSystem.GetDirectoryInfo("desktop");
 
-            if (curDir == null)
+
+            try
             {
                 curDir = FileSystem.GetDirectoryInfo("documents");
             }
+            catch( DirectoryNotFoundException ex)
+            {
+                // If both desktop and documents folders are not found, save to captured_packets folder in current directory
+                Console.WriteLine(" Could not find desktop or documents folder, saving to current directory.");
+                FileSystem.CreateDirectory("captured_packets");
+                curDir = FileSystem.GetDirectoryInfo("captured_packets");
+            }
+               
 
+            
 
             Console.WriteLine(" Stopping packet capture...");
-            Console.WriteLine($" Logging captured packets to a location {curDir} ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($" Logging captured packets to a location (\"{curDir}\") ");
 
             _IsCancelled = true;
-            FileSystem.WriteAllText(Path.Combine(curDir.FullName, "captured_packets_log.pcap"));
+
+            try
+            {
+                // Tries to save the captured data to a file without appending
+                FileSystem.WriteAllText(
+                    Path.Combine(curDir.FullName, "captured_packets_log.pcap"), $"", false);
+            }
+            catch (UnauthorizedAccessException uaEx)
+            {
+                // Handle unauthorized access exception
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($" Access denied saving the data to a file | ERROR : {uaEx.Message} |");
+            }
+            catch (IOException ioEx)
+            {
+                // Handle I/O exception
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($" I/O error occured saving the data to a file | ERROR : {ioEx.Message} |");
+                
+            }
+            catch (Exception ex)
+            {
+                // Handle any other exceptions
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($" Unexpected error occured saving the data to a file | ERROR : {ex.Message} |");
+              
+            }
+            finally
+            {
+                // Exit the application gracefully
+                e.Cancel = false;
+                Console.WriteLine(" Packet capture stopped. Exiting application...");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Environment.Exit(0);
+
+            }
 
 
+        }
+        public static void MagicMethod(PcapDotNet.Packets.Packet packet)
+        {
+           
+            IpV4Datagram ipv4Packet = packet.Ethernet.IpV4;
+            IpV6Datagram ipv6Packet = packet.Ethernet.IpV6;
+
+            if (ipv4Packet != null)
+            {
+                if (ipv4Packet.Protocol == IpV4Protocol.Tcp)
+                {
+                    var tcp = ipv4Packet.Tcp;
+                    Console.WriteLine($" TCP Packet | Source : {ipv4Packet.Source} | Destination : {ipv4Packet.Destination} | Source Port : {tcp.SourcePort} | Destination Port : {tcp.DestinationPort} |");
+                }
+                else if (ipv4Packet.Protocol == IpV4Protocol.Udp)
+                {
+                    var udp = ipv4Packet.Udp;
+                    Console.WriteLine($" UDP Packet | Source : {ipv4Packet.Source} | Destination : {ipv4Packet.Destination} | Source Port : {udp.SourcePort} | Destination Port : {udp.DestinationPort} |");
+                }
+                else
+                {
+                    Console.WriteLine($" IPv4 Packet | Source : {ipv4Packet.Source} | Destination : {ipv4Packet.Destination} | Protocol : {ipv4Packet.Protocol} |");
+                }
+
+            }
+            else if (ipv6Packet != null)
+            {
+                Console.WriteLine(" Is ipv6");
+            }
+            else
+            {
+                Console.WriteLine(" Non ip packet");
+            }
         }
         public static void AlertOnSuspiciousActivity()
         {
